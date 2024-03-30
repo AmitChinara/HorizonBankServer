085a5e0fe585785d728657f18903ce417a03e030
Amit Chinara <amitchinara@gmail.com>
2024-03-31 03:26:23 +0530

    Added input field for Create Account

--- Data.py
+++ Data.py
@@ -7,16 +7,28 @@ class Data:
     ACCOUNT</p></div></div> </div>"""
 
     create_account_form = """<div class="form-container">
-    <div class="form-division" id="upper">
-    <div class="sub-division upper-sub-division" id="upper-division-1"></div>
-    <div class="sub-division upper-sub-division" id="upper-division-2"></div>
-    </div>
-    <div class="form-division" id="lower">
-    <div class="sub-division lower-sub-division" id="lower-division-1"></div>
-    <div class="sub-division lower-sub-division" id="lower-division-2"></div>
-    </div>
-    </div>"""
+                    <div class="form-division" id="upper">
+                        <div class="sub-division upper-sub-division" id="upper-division-1"><div class="create-account-input-container"></div></div>
+                        <div class="sub-division upper-sub-division" id="upper-division-2"><div class="create-account-input-container"></div></div>
+                    </div>
+                    <div class="form-division" id="lower">
+                        <div class="sub-division lower-sub-division" id="lower-division-1">
+                            <div class="buttons" id="submit-button"><p>SUBMIT</p></div>
+                        </div>
+                        <div class="sub-division lower-sub-division" id="lower-division-2">
+                            <div class="buttons" id="clear-button" onclick="createAccountClear()"><p>CLEAR</p></div>
+                        </div>
+                    </div>
+                </div>"""
 
     account_login_info = """ """
 
     delete_account_form = """ """
+
+    create_account_left_input = """<div class="input-container" id="input-field-1"><input type="text" id="first-name" class="input-field" placeholder="FIRST NAME"></div>
+                            <div class="input-container" id="input-field-2"><input type="text" id="last-name" class="input-field" placeholder="LAST NAME"></div>
+                            <div class="input-container" id="input-field-3"><input type="email" id="email-id" class="input-field" placeholder="EMAIL ID"></div>"""
+
+    create_account_right_input = """<div class="input-container" id="input-field-4"><input type="date" class="input-field" id="date-of-birth"></div>
+                            <div class="input-container" id="input-field-5"><input type="password" id="pin" class="input-field" placeholder="PIN" oninput="checkPin(1)"></div>
+                            <div class="input-container" id="input-field-6"><input type="password" id="confirm-pin" class="input-field" placeholder="CONFIRM PIN" oninput="checkPin(2)"></div>"""
--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ CORS(app)
 @app.route('/')
 def pageData():
     data = {f'main_page_info': f'{Data.main_page_info}', f'create_account_form': f'{Data.create_account_form}',
-            f'account_login_info': f'{Data.account_login_info}', f'delete_account_form': f'{Data.delete_account_form}'}
+            f'account_login_info': f'{Data.account_login_info}', f'delete_account_form': f'{Data.delete_account_form}', f'create_account_left_input': f'{Data.create_account_left_input}', f'create_account_right_input': f'{Data.create_account_right_input}'}
     return jsonify(data)
 
 
